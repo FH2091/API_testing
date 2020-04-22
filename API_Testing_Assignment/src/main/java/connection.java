b45c57461cc6653e04e92f7d14189926b961de16
AbdelrahmanGamal1998 <dofdaa_1998@yahoo.com>
2020-04-23 01:32:40 +0200

    ta2sema connection

--- API_Testing_Assignment/src/main/java/connection.java
+++ API_Testing_Assignment/src/main/java/connection.java
@@ -1,6 +1,7 @@
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -19,14 +20,31 @@ public class connection {
 	 
 	}
 	 
-public static void call_me(String country) throws Exception {
 
-    
-
-   //  String country =getname();
-     String f="https://restcountries.eu/rest/v2/name/";
-     String spec =f+country+ "?fields=name;capital;population";
-     
+	public static String Bind_Url() {
+		String country=getname();
+	   String f="https://restcountries.eu/rest/v2/name/";
+	     String spec =f+country+ "?fields=name;capital;population";
+		return spec;
+	}
+	
+	
+	
+	public static String Read_From_API(HttpURLConnection con) throws IOException {
+	 BufferedReader in = new BufferedReader(
+		       new InputStreamReader(con.getInputStream()));
+		     String inputLine;
+		    
+		     while ((inputLine = in.readLine()) != null) {
+		     	response.append(inputLine);
+		     }
+		     
+		     in.close();
+			return inputLine;
+	}
+	
+	public static void call_me(String country) throws Exception {
+     String spec=Bind_Url();
      URL obj = new URL(spec);
      HttpURLConnection con = (HttpURLConnection) obj.openConnection();
   
@@ -36,18 +54,8 @@ public static void call_me(String country) throws Exception {
      int responseCode = con.getResponseCode();
      System.out.println("\n Sending a request to API : " + spec);
      System.out.println("Response Code : " + responseCode);
-     BufferedReader in = new BufferedReader(
-       new InputStreamReader(con.getInputStream()));
-     String inputLine;
-    
-     while ((inputLine = in.readLine()) != null) {
-     	response.append(inputLine);
-     }
-     
-     in.close();
-     
+     Read_From_API(con);
    
-    	 
      }
 
 public String getresponse() {
